54acaad338986ba1334e3c9d47cc7388de916151
nick frost <nfrost360@gmail.com>
2024-03-06 03:11:18 +0000

    Day 4

--- myApp.js
+++ myApp.js
@@ -1,15 +1,43 @@
 let express = require('express');
 require('dotenv').config();
 let app = express();
+let bodyParser = require('body-parser');
+
+//#7
+app.use((req, res, next) => {
+    console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+    next();
+})
+
+//#11
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//#8 
+app.get("/now", (req, res, next) => { 
+    req.time = new Date().toString();
+    next()
+}, (req, res) => {
+    res.json({'time' : req.time})
+})
+
+//#9
+app.get('/:word/echo', (req, res) => {
+    res.json({echo : req.params.word});
+})
+
+//#10
+app.get('/name', (req, res) => {
+    let string = req.query.first + ' ' + req.query.last; 
+    res.json({name: string});
+})
 
 //#1
-//console.log("Hello World");
-
+console.log("Hello World"); 
 
 //#2
-// app.get("/", (req, res) => {
-//     res.send("Hello Express")
-// })
+ app.get("/", (req, res) => {
+    res.send("Hello Express")
+})
 
 //#4
 app.use("/public", express.static(__dirname + "/public"));
@@ -21,10 +49,9 @@ app.get("/", (req, res) => {
 
 
 //#5 
-//app.get("/json", (req, res) => {
- //   res.json({"message": "Hello json"});
-//})
-console.log(process.env.MESSAGE_STYLE)
+app.get("/json", (req, res) => {
+    res.json({"message": "Hello json"});
+})
 //#6
 app.get("/json", (req, res) => {
 if(process.env.MESSAGE_STYLE === 'uppercase') {
@@ -33,22 +60,12 @@ if(process.env.MESSAGE_STYLE === 'uppercase') {
         res.json({"message": "Hello json"});
     } 
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ //#12 
+app.post('/name', bodyParser.urlencoded({ extended: false }),
+    (req, res) => {
+        let string = req.body.first + " " + req.body.last;
+        res.json({ name: string });
+    })  
 
 
 
